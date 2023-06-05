65cfe25ffedc633376a2e1d4dc08040a9e57fc32
MariaSevil <125469456+MariaSevil@users.noreply.github.com>
2023-06-05 19:01:27 +0300

    Task 3

---
+++ Lesson_5/L3.py
@@ -0,0 +1,20 @@
+""" Напишите функцию, которая принимает одно число и
+проверяет, является ли оно простым
+Напоминание: Простое число - это число, которое
+имеет 2 делителя: 1 и n(само число)
+ """
+ 
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+num = int(input("Введите число: "))
+
+if is_prime(num):
+    print("yes")
+else:
+    print("no")
